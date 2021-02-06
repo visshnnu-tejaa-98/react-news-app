6c38044208ce096939758d7456272e54e42ead14
visshnnu tejaa <visshnnutejaa@gmail.com>
2021-02-06 17:33:56 +0530

    social media buttons added

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,6 +2,9 @@ const Footer = () => {
 	return (
 		<div className='footer mt-5'>
 			<p>All rights reserverd &copy; {new Date().getFullYear()}</p>
+			<i className='fab fa-facebook-f m-3'></i>
+			<i className='fab fa-twitter m-3'></i>
+			<i className='fab fa-instagram m-3'></i>
 		</div>
 	);
 };
